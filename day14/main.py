f616be22a692263faf8fa0d8f81bbdffe2a721f5
Havik Priyatama <priyatama.havik.office@gmail.com>
2022-07-08 23:04:30 +0700

    done day14

--- day14/main.py
+++ day14/main.py
@@ -1,12 +1,16 @@
+import os
+
 from art import logo, vs
 from data import data
 import random
 
-win_count = 0
-
 
 def get_data():
     return random.choice(data)
+
+
+def format_data(account):
+    return f"{account['name']}, {account['description']}, {account['country']}"
 
 
 def compare(a, b, c):
@@ -18,21 +22,31 @@ def compare(a, b, c):
         return False
 
 
-def show_score():
-    if win_count > 0:
-        print(f"You are right current score {win_count}")
-
-
-a = get_data()
-b = get_data()
-show_score()
+# def show_score():
+#     print(f"You are right current score {win_count}")
+#     print(f"Sorry, that's wrong, final score {win_count}")
 print(logo)
-print(f"Compare A : {a['name']}, {a['description']}, {a['country']}")
-print(vs)
-print(f"Against B : {b['name']}, {b['description']}, {b['country']}")
-user_choice = input("Who has more followers? Type 'A' or 'B'? ").lower()
-yes = compare(a=a["follower_count"], b=b["follower_count"], c=user_choice)
-if yes:
-    print("you win")
-else:
-    print("you lose")
+win_count = 0
+game_should_continue = True
+b = get_data()
+
+while game_should_continue:
+    a = b
+    b = get_data()
+    while a == b:
+        b = get_data()
+
+    print(f"Compare A : {format_data(a)}")
+    print(vs)
+    print(f"Against B : {format_data(b)}")
+    user_choice = input("Who has more followers? Type 'A' or 'B'? ").lower()
+    yes = compare(a=a["follower_count"], b=b["follower_count"], c=user_choice)
+    os.system("cls")
+    print(logo)
+    if yes:
+        win_count += 1
+        print(f"you win, current score {win_count}")
+    else:
+        print(f"you lose, final score {win_count}")
+        game_should_continue = False
+
