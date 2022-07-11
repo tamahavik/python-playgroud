cc4657d7eab65f93c5b4ba4c53c87e1371ca8bcf
Havik Priyatama <59886@fif.co.id>
2022-07-11 11:14:31 +0700

    finish day16

--- day16/menu.py
+++ day16/menu.py
@@ -1,17 +1,9 @@
-class MenuItem:
-    """Models each Menu Item."""
-    def __init__(self, name, water, milk, coffee, cost):
-        self.name = name
-        self.cost = cost
-        self.ingredients = {
-            "water": water,
-            "milk": milk,
-            "coffee": coffee
-        }
+from menu_item import MenuItem
 
 
 class Menu:
     """Models the Menu with drinks."""
+
     def __init__(self):
         self.menu = [
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
