8ce9999377662e4f1772f9ddd9d5b8f40c4164fe
Havik Priyatama <59886@fif.co.id>
2022-07-12 23:03:33 +0700

    complete day20 and day21 : snake game

---
+++ day20/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.speed("fastest")
+        self.show()
+
+    def show(self):
+        self.write(f"Score : {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.score += 1
+        self.clear()
+        self.show()
